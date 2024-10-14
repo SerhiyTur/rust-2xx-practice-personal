b818245ff5636859f23a33d38ecd140e0526b964
Serhiy Tur <s-tur@e-u.edu.ua>
2024-10-14 22:51:02 +0300

    Added solution for task X

--- src/test3.rs
+++ src/test3.rs
@@ -1,3 +1,4 @@
+#[test]
 fn test31() {
     let x: i32 = 5; // Uninitialized but used, ERROR !
     let _y: i32; // Uninitialized but also unused, only a Warning !
@@ -5,6 +6,19 @@ fn test31() {
     assert_eq!(x, 5);
     println!("Success!");
 }
-fn test32(){
+#[test]
+fn test32() {
+    // Оголошуємо змінну і робимо її змінною
+    let mut x = 1;
 
+    // Змінюємо значення змінної
+    x += 2;
+
+    // Перевіряємо результат
+    assert_eq!(x, 3);
+
+    // Виводимо повідомлення про успіх
+    println!("Success!");
 }
+
+
