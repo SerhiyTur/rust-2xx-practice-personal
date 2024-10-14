01ed1ba669f20a884cc99b5b1aea562ae44c64d5
Ваше Ім'я <s-tur@e-u.edu.ua>
2024-10-14 19:54:22 +0300

    Fixed author identity issue

---
+++ src/test3.rs
@@ -0,0 +1,10 @@
+fn test31() {
+    let x: i32 = 5; // Uninitialized but used, ERROR !
+    let _y: i32; // Uninitialized but also unused, only a Warning !
+
+    assert_eq!(x, 5);
+    println!("Success!");
+}
+fn test32(){
+
+}
---
+++ target/debug/deps/rust_2xx_practice-85a535bd4f74186d.d
@@ -0,0 +1,10 @@
+C:\Users\Admin\RustroverProjects\rust-2xx-practice-personal\target\debug\deps\rust_2xx_practice-85a535bd4f74186d.exe: src/main.rs src\chapter3.rs src\chapter4.rs src\chapter5.rs src\chapter6.rs src\test3.rs
+
+C:\Users\Admin\RustroverProjects\rust-2xx-practice-personal\target\debug\deps\rust_2xx_practice-85a535bd4f74186d.d: src/main.rs src\chapter3.rs src\chapter4.rs src\chapter5.rs src\chapter6.rs src\test3.rs
+
+src/main.rs:
+src\chapter3.rs:
+src\chapter4.rs:
+src\chapter5.rs:
+src\chapter6.rs:
+src\test3.rs:
