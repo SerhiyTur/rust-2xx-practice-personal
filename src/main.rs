cf305e75d3e1d142116b0cef9a85b01a187c429f
Serhiy Tur <s-tur@e-u.edu.ua>
2024-10-16 10:18:44 +0300

    Add .idea and target to .gitignore

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod chapter4;
 mod chapter5;
 mod chapter6;
 mod test3;
+mod romb;
 
 fn main() {
     println!("Hello, world!");
--- target/debug/deps/rust_2xx_practice-85a535bd4f74186d.d
+++ target/debug/deps/rust_2xx_practice-85a535bd4f74186d.d
@@ -1,6 +1,6 @@
-C:\Users\Admin\RustroverProjects\rust-2xx-practice-personal\target\debug\deps\rust_2xx_practice-85a535bd4f74186d.exe: src/main.rs src\chapter3.rs src\chapter4.rs src\chapter5.rs src\chapter6.rs src\test3.rs
+C:\Users\Admin\RustroverProjects\rust-2xx-practice-personal\target\debug\deps\rust_2xx_practice-85a535bd4f74186d.exe: src/main.rs src\chapter3.rs src\chapter4.rs src\chapter5.rs src\chapter6.rs src\test3.rs src\romb.rs
 
-C:\Users\Admin\RustroverProjects\rust-2xx-practice-personal\target\debug\deps\rust_2xx_practice-85a535bd4f74186d.d: src/main.rs src\chapter3.rs src\chapter4.rs src\chapter5.rs src\chapter6.rs src\test3.rs
+C:\Users\Admin\RustroverProjects\rust-2xx-practice-personal\target\debug\deps\rust_2xx_practice-85a535bd4f74186d.d: src/main.rs src\chapter3.rs src\chapter4.rs src\chapter5.rs src\chapter6.rs src\test3.rs src\romb.rs
 
 src/main.rs:
 src\chapter3.rs:
@@ -8,3 +8,4 @@ src\chapter4.rs:
 src\chapter5.rs:
 src\chapter6.rs:
 src\test3.rs:
+src\romb.rs:
